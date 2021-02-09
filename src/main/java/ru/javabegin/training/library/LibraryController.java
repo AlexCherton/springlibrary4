5a4f535ec831672ca85f31d1a33a12063fc2ebfa
AlexCherton <Alex_General2002@mail.ru>
2021-02-09 03:01:59 +0300

    New commit

--- src/main/java/ru/javabegin/training/library/LibraryController.java
+++ src/main/java/ru/javabegin/training/library/LibraryController.java
@@ -2,6 +2,8 @@ package ru.javabegin.training.library;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LibraryController {
@@ -9,4 +11,5 @@ public class LibraryController {
     public String sayHello() {
         return "springlibrary";
     }
+
 }
